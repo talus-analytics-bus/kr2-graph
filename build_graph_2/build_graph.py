c1a3ecc3b457dec8c818abc77d2bfd0354fbd04d
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-04-29 21:52:35 +0000

    Logging changes and code structure cleanup

--- build_graph_2/build_graph.py
+++ build_graph_2/build_graph.py
@@ -1,6 +1,6 @@
 import os
 import csv
-import logging
+from loguru import logger
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
 import time
@@ -13,10 +13,6 @@ NEO4J_AUTH = (os.getenv("NEO4J_USER"), os.getenv("NEO4J_PASS"))
 NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_DRIVER = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
 SESSION = NEO4J_DRIVER.session()
-
-# subgraph = SESSION.run("MATCH (n) RETURN n LIMIT 10")
-
-logging.basicConfig(filename='build_graph_debug.log', encoding='utf-8', level=logging.DEBUG)
 
 
 def read_dons_csv():
@@ -31,9 +27,7 @@ def get_dons_disease_set(rows):
     dons_diseases = set()
     for row in rows:
         if row["DiseaseLevel1"] == "Influenza A" and row["DiseaseLevel2"] != "NA":
-            dons_diseases.add(
-                row["DiseaseLevel1"] + "|" + row["DiseaseLevel2"]
-            )
+            dons_diseases.add(row["DiseaseLevel1"] + "|" + row["DiseaseLevel2"])
 
     return dons_diseases
 
@@ -43,23 +37,23 @@ def merge_lineage(metadata):
     pass
 
 
-rows = read_dons_csv()
-keys = get_dons_disease_set(rows)
+if __name__ == "__main__":
+    rows = read_dons_csv()
+    keys = get_dons_disease_set(rows)
 
-for key in keys:
-    Disease1, Disease2 = key.split("|")
-    print("\n", Disease2.strip())
+    for key in keys:
+        Disease1, Disease2 = key.split("|")
+        ncbi_id = ncbi.id_search(Disease2)
 
-    ncbi_id = ncbi.id_search(Disease2)
+        if not ncbi_id:
+            ## save broken search terms to file
+            with open("not_found.txt", "a") as f:
+                f.write(f"{Disease1}, {Disease2}")
+                f.write("\n")
+                f.close()
 
-    if not ncbi_id:
-        ## save broken search terms to file
-        with open('not_found.txt', 'a') as f:
-            f.write(f'{Disease1}, {Disease2}')
-            f.write('\n')
-            f.close()
-
-    time.sleep(.4)
+        # resepect api rate limit
+        time.sleep(0.4)
 
 # print(ncbi.id_search('H5N6'))
 
