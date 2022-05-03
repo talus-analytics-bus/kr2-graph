184d7360042001962c0fee4421d3e379540a7bbb
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-05-03 14:07:47 +0000

    Merge taxonomy response from NCBI

--- build_graph_2/build_graph.py
+++ build_graph_2/build_graph.py
@@ -3,8 +3,9 @@ import csv
 from pprint import pprint
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
+from loguru import logger
 
-# import time
+import time
 
 import ncbi
 
@@ -27,38 +28,87 @@ def read_dons_csv():
 def get_dons_disease_set(rows):
     dons_diseases = set()
     for row in rows:
-        if row["DiseaseLevel1"] == "Influenza A" and row["DiseaseLevel2"] != "NA":
+        # if row["DiseaseLevel1"] == "Influenza A" and row["DiseaseLevel2"] != "NA":
+        if row["DiseaseLevel2"] != "NA":
             dons_diseases.add(row["DiseaseLevel1"] + "|" + row["DiseaseLevel2"])
+        else:
+            dons_diseases.add(row["DiseaseLevel1"] + "|")
 
     return dons_diseases
 
 
-# merge ncbi lineage into graph
-def merge_lineage(metadata):
-    pass
+def merge_taxon(taxon):
+    pprint(taxon["ScientificName"])
+    rank = taxon["Rank"].replace(" ", "_")
+    SESSION.run(
+        f'MERGE (n:{rank} {{name: "{taxon["ScientificName"]}", '
+        f'  Rank: "{rank}", '
+        f'  TaxId: "{taxon["TaxId"]}" '
+        f"}})"
+    )
+
+
+def merge_taxon_link(parent, child):
+    parent_rank = parent["Rank"].replace(" ", "_")
+    child_rank = child["Rank"].replace(" ", "_")
+    SESSION.run(
+        f'MATCH (parent:{parent_rank} {{name: "{parent["ScientificName"]}"}}), '
+        f'  (child:{child_rank} {{name: "{child["ScientificName"]}"}}) '
+        f"MERGE (parent)-[r:CONTAINS]->(child) "
+    )
+
+
+def merge_lineage(lineage):
+    for index, taxon in enumerate(lineage):
+        # merge parent node
+        merge_taxon(taxon)
+
+        # if there are children
+        if index + 1 < len(lineage):
+            child = lineage[index + 1]
+            # merge child node
+            merge_taxon(child)
+            # merge relationship
+            merge_taxon_link(taxon, child)
+
+
+def merge_ncbi_metadata_response(ncbi_id, ncbi_metadata):
+    merge_taxon({**ncbi_metadata, "TaxId": ncbi_id})
+
+    merge_taxon(ncbi_metadata["LineageEx"][-1])
+
+    merge_taxon_link(
+        ncbi_metadata["LineageEx"][-1], {**ncbi_metadata, "TaxId": ncbi_id}
+    )
+
+    merge_lineage(ncbi_metadata["LineageEx"])
 
 
 if __name__ == "__main__":
-    # rows = read_dons_csv()
-    # keys = get_dons_disease_set(rows)
+    rows = read_dons_csv()
+    keys = get_dons_disease_set(rows)
 
-    # for key in keys:
-    #     Disease1, Disease2 = key.split("|")
-    #     ncbi_id = ncbi.id_search(Disease2)
+    for key in keys:
+        Disease1, Disease2 = key.split("|")
+        ncbi_id = ncbi.id_search(f"{Disease1} {Disease2}")
 
-    #     # if not ncbi_id:
-    #     #     ## save broken search terms to file
-    #     #     with open("not_found.txt", "a") as f:
-    #     #         f.write(f"{Disease1}, {Disease2}")
-    #     #         f.write("\n")
-    #     #         f.close()
+        if ncbi_id:
+            ncbi_metadata = ncbi.get_metadata(ncbi_id)
+            merge_ncbi_metadata_response(ncbi_id, ncbi_metadata)
 
-    #     # resepect api rate limit
-    #     time.sleep(0.4)
+        else:
+            ## save broken search terms to file
+            with open("not_found.txt", "a") as f:
+                f.write(f"{Disease1}, {Disease2}")
+                f.write("\n")
+                f.close()
 
-    ncbi_id = ncbi.id_search("H9N2")
-    ncbi_metadata = ncbi.get_metadata(ncbi_id)
-    pprint(ncbi_metadata)
+        # resepect api rate limit
+        time.sleep(0.4)
+
+# ncbi_id = ncbi.id_search("anthrax")
+# ncbi_metadata = ncbi.get_metadata(ncbi_id)
+
 
 # print(get_dons_disease_set(read_dons_csv()))
 
